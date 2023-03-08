643e1851f924697432e06d030d148a4e8d1aafc9
Fatima <112809793+fatimazen@users.noreply.github.com>
2023-03-08 14:02:50 +0100

    ajout like

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,8 +10,10 @@ use DateTimeImmutable;
 use App\Entity\Comment;
 use App\Entity\Ingredient;
 use App\Entity\Newsletter;
+use App\Entity\RecipeIngredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\Query\AST\LikeExpression;
 
 class AppFixtures extends Fixture
 {
@@ -24,77 +26,135 @@ class AppFixtures extends Fixture
         for($i = 0; $i < 10; $i++)
         {
 
-        $user = new User;
+        $user = new User();
         $user 
-        -> setUsername($faker->firstName().''.$faker->lastName())
-        ->setAvatar($faker->imageUrl(640, 480, 'person', true))
-        ->setEmail($faker->safeEmail())
-        ->setPassword($faker->sha256())
-        ->setIsSubscribed($faker->boolean())
-        ->setRoles(['ROLE_USER'])
+            -> setUsername($faker->firstName().''.$faker->lastName())
+            ->setAvatar($faker->imageUrl(640, 480, 'person', true))
+            ->setEmail($faker->safeEmail())
+            ->setPassword($faker->sha256())
+            ->setIsSubscribed($faker->boolean())
+            ->setRoles(['ROLE_USER'])
+            
     ;
             $users []= $user;
         $manager->persist($user);
     }
 
+    $summerStart = new \DateTimeImmutable('2023-06-21');
+    $fallStart = new \DateTimeImmutable('2023-09-23');
+    $springStart = new \DateTimeImmutable('2023-03-20');
+    $winterStart = new \DateTimeImmutable('2023-12-21');
+    
+    $summer = new Season;
+    $summer
+    ->setName('Summer')
+    ->setStartAt($summerStart)
+    ->setEndAt($fallStart)
+    ;
+    $manager->persist($summer);
+    
+    $spring = new Season;
+    $spring
+    ->setName('Spring')
+    ->setStartAt($springStart)
+    ->setEndAt($summerStart)
+    ;
+    $manager->persist($spring);
+    
+    $winter = new Season;
+    $winter
+    ->setName('Winter')
+    ->setStartAt($winterStart)
+    ->setEndAt($springStart)
+    ;
+    $manager->persist($winter);
+    
+    $fall = new Season;
+    $fall
+    ->setName('Fall')
+    ->setStartAt($fallStart)
+    ->setEndAt($winterStart)
+    ;
+    $manager->persist($fall);
+    
+    $seasons = [$summer,$spring,$winter,$fall];
+    
+    $ingredients=[];
     for($i = 0; $i < 10; $i++)
     {
-        $ingredient = new Ingredient;
+        $ingredient = new Ingredient();
         $ingredient
-        ->setName($faker->word())
-        ->setType($faker->word())
-        ->setDescription($faker->text())
-        ->setImage($faker->imageUrl(640, 480, 'ingredient', true))
+            ->setName($faker->words(10, true))
+            ->setType($faker->randomElement(['fruit','legume']))
+            ->setDescription($faker->text())
+            ->setImage($faker->imageUrl(640, 480, 'food', true))
+            ->addSeason($faker->randomElement($seasons))
+        
         ;
         $manager->persist($ingredient);
+        $ingredients[]=$ingredient;
+    
     }
        
-
-    for($i = 0; $i < 10; $i++)
-{
-    $season = new Season;
-    $season
-    ->setName($faker->word())
-    ->setStartAt(DateTimeImmutable::createFromMutable($faker->dateTime()))
-    ->setEndAt(DateTimeImmutable::createFromMutable($faker->dateTime()))
-    ;
-    $manager->persist($season);
-}
     $recipes = [];
-for($i = 0; $i < 10; $i++)
+    for($i = 0; $i < 10; $i++)      
 {
-    $recipe = new Recipe;
+    $recipe = new Recipe();
     $recipe
-    ->setName($faker->word())
-    ->setDescription($faker->text())
-    ->setLevel($faker->randomNumber())
-    ->setImage($faker->imageUrl(640, 480, 'recipe', true))
-    ;
+        ->setName($faker->words(20, true))
+        ->setDescription($faker->text())
+        ->setLevel($faker->numberBetween(1, 3))
+        ->setImage($faker->imageUrl(640, 480, 'recipe', true))
+        ->addseason($faker->randomElement($seasons))
+        ->addUser($faker->randomElement($users))
+        ;
+        $recipes [] = $recipe;
     $manager->persist($recipe);
-    $recipes [] = $recipe;
 }
- for ($i = 0; $i < 10; $i++)
+    for ($i = 0; $i < 30; $i++)
+    $comments=[];
  {
-    $comment = new Comment;
+    $comment = new Comment();
     $comment
-    ->setContent($faker->text())
-    ->setUser($faker->randomElement($users))
-    ->setRecipe($faker->randomElement($recipes))
+        ->setContent($faker->text(60))
+        ->setUser($faker->randomElement($users))
+        ->setRecipe($faker->randomElement($recipes))
     
     ;
+    $comments[]=$comment;
     $manager->persist($comment);
  }
- for ($i = 0; $i <10; $i++)
+    for ($i = 0; $i <10; $i++)
  {
-    $newsletter = new Newsletter;
+    $newsletter = new Newsletter();
     $newsletter
-    ->setContent($faker->text())
-    ->setSendAt(DateTimeImmutable::createFromMutable($faker->dateTime()))
+    ->setContent($faker->sentence(2))
+    ->setSendAt(dateTimeImmutable::createFromMutable($faker->dateTime()))
+    ->addRecipe($faker->randomElement($recipes))
+    ->addRecipe($faker->randomElement($recipes))
+    ->addRecipe($faker->randomElement($recipes))
+    ->addRecipe($faker->randomElement($recipes))
     
     ;
     $manager->persist($newsletter);
  }
 
+    $recipeIngredients= [];
+    for ($i = 0; $i <30; $i++)
+ {
+    $recipeIngredient = new RecipeIngredient();
+    $recipeIngredient
+    ->setRecipe($faker->randomElement($recipes))
+    ->setIngredient($faker->randomElement($ingredients))
+    ->setQuantity($faker->randomDigitNotNull().$faker->randomElement(['kg', 'gr', 'l', 'ml']))
+    
+    ;
+    $manager->persist($recipeIngredient);
+    $recipeIngredients[]=$recipeIngredient;
+
+ }
+
     $manager->flush();
+
 }
 }
